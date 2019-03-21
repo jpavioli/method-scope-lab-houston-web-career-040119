414862ca145d9085547db4d3ee4893c495f13c5f
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-21 15:26:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,4 +1,5 @@
-phrase = "It's-a me, Mario!"
-def catch_phrase(phrase)
+#catch_phrase puts out a catchphrase that cant be called outside the method
+def catch_phrase
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
